28902fc24b5e166ec4a983f274d76bdde5a3cacf
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-05-15 12:27:52 +0200

    nothing

--- Azule - Python rewrite.py
+++ Azule - Python rewrite.py
@@ -20,10 +20,9 @@ def extract_deb(deb_file, output_dir):
         subprocess.call(['dpkg-deb', '-x', deb_file, output_dir])
     elif platform.system() == 'Darwin':
         subprocess.call(['sudo', 'ditto', '-x', '-k', deb_file, output_dir])
-    elif platform.system() == 'Windows':
-        subprocess.call(['7z', 'x', '-y', '-odefaultdir', deb_file])
+    #elif platform.system() == 'Windows':
+        # yeah idk - manual extraction needed
 
-# Funktion zum Suchen von Dateien, die mit ".dylib" enden
 def find_dylib_files(path):
     dylib_files = []
     for root, dirs, files in os.walk(path):
@@ -32,17 +31,12 @@ def find_dylib_files(path):
                 dylib_files.append(os.path.join(root, file))
     return dylib_files
 
-# Pfad zur .deb-Datei
 deb_file_path = 'path/to/deb_file.deb'
 
-# Ausgabeordner für die extrahierten Dateien
 output_dir = 'path/to/output_dir'
 
-# Extrahieren der .deb-Datei in den Ausgabeordner
 extract_deb(deb_file_path, output_dir)
 
-# Suchen nach Dateien, die mit ".dylib" enden, im Ausgabeordner
 dylib_files = find_dylib_files(output_dir)
 
-# Drucken der gefundenen Dateien
 print(dylib_files)
