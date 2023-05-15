c76017632b1bc2e258d5582825142a1d0d409a3e
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-05-15 13:10:54 +0200

    added flag parser

--- Azule - Python rewrite.py
+++ Azule - Python rewrite.py
@@ -2,19 +2,47 @@ import os
 import shutil
 import subprocess
 import platform
+import argparse
 
-print("[-i] Specify the IPA to patch")
+
+
+
+
+
+print("[-i] Specify the the IPA to patch")
 print("[-o] Specify an output directory")
 print("[-f] Specify the files or tweaks")
-print("[-n] Specify a name for the Output IPA")
+print("[-n] Specify a name for the Output iPA")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--arg1', nargs='+', help='Erstes Argument')
+parser.add_argument('-i', '--arg2', nargs='+', help='Zweites Argument')
+parser.add_argument('-r', '--arg3', nargs='+', help='Drittes Argument')
+
+# Benutzereingabe abfragen
+flags = input("Gib die Flags mit den Werten ein: ")
+
+# args parsen
+parsed_args, unknown = parser.parse_known_args(flags.split())
+
+arg1 = parsed_args.arg1 if parsed_args.arg1 else []
+arg2 = parsed_args.arg2 if parsed_args.arg2 else []
+arg3 = parsed_args.arg3 if parsed_args.arg3 else []
+
+print("Erstes Argument:", arg1)
+print("Zweites Argument:", arg2)
+print("Drittes Argument:", arg3)
+
+
 
 def extract_deb(deb_file, output_dir):
     if platform.system() == 'Linux':
         subprocess.call(['dpkg-deb', '-x', deb_file, output_dir])
     elif platform.system() == 'Darwin':
         subprocess.call(['sudo', 'ditto', '-x', '-k', deb_file, output_dir])
-    # elif platform.system() == 'Windows':
-    # yeah idk - manual extraction needed
+    #elif platform.system() == 'Windows':
+        # yeah idk - manual extraction needed
 
 def find_dylib_files(path):
     dylib_files = []
@@ -24,18 +52,12 @@ def find_dylib_files(path):
                 dylib_files.append(os.path.join(root, file))
     return dylib_files
 
-deb_file_path = '/home/bsnw/Downloads/com.ps.ytuhd_1.3.5-1_iphoneos-arm64(1).deb'
-output_dir = '/home/bsnw/Downloads'
+deb_file_path = 'path/to/deb_file.deb'
 
-extracted_dir = os.path.join(output_dir, 'extracted_deb')
-extract_deb(deb_file_path, extracted_dir)
+output_dir = 'path/to/output_dir'
 
-dylib_files = find_dylib_files(extracted_dir)
+extract_deb(deb_file_path, output_dir)
 
-temp_dir = os.path.join(output_dir, 'temp')
-os.makedirs(temp_dir, exist_ok=True)
-
-for dylib_file in dylib_files:
-    shutil.copy(dylib_file, temp_dir)
+dylib_files = find_dylib_files(output_dir)
 
 print(dylib_files)
