336cf422edd0099f890696e64e5637b69a23eb7e
binnichtaktiv <96953964+binnichtaktiv@users.noreply.github.com>
2023-05-16 23:28:07 +0200

    better deb extraction on every os - 7z or dpkg needed tho

--- Azule - Python rewrite.py
+++ Azule - Python rewrite.py
@@ -3,12 +3,11 @@ import shutil
 import subprocess
 import platform
 import time
+import patoolib
 
-#trash
 
 deb_file = r"C:\Users\jonas\Downloads\Telegram Desktop\com.dvntm.youtubeplus_2.3_iphoneos-arm.deb"
 output_dir = r"C:\Users\jonas\Desktop"
-#trash
 
 print("[-i] Specify the IPA to patch")
 print("[-o] Specify an output directory")
@@ -16,16 +15,27 @@ print("[-f] Specify the files or tweaks")
 print("[-n] Specify a name for the Output IPA")
 
 def extract_deb(deb_file, output_dir):
-    if platform.system() == 'Linux':
-        subprocess.call(['dpkg-deb', '-x', deb_file, output_dir])
-    elif platform.system() == 'Darwin':
-        subprocess.call(['sudo', 'ditto', '-x', '-k', deb_file, output_dir])
-    elif platform.system() == 'Windows':
-        subprocess.call([r"C:\Program Files\7-Zip\7z.exe", "x", "-y", f"-o{output_dir}", deb_file])
-        time.sleep(2)
-        data_tar = output_dir + "\data.tar"
-        subprocess.call([r"C:\Program Files\7-Zip\7z.exe", "x", "-y", f"-o{output_dir}", data_tar])
-        os.remove(data_tar)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    patoolib.extract_archive(deb_file, outdir=output_dir)
+
+    data_file = None
+    for file in os.listdir(output_dir):
+        if file.startswith("data.tar"):
+            data_file = os.path.join(output_dir, file)
+            break
+
+    if data_file:
+        patoolib.extract_archive(data_file, outdir=output_dir)
+        os.remove(data_file)
+    else:
+        pass
+
+deb_file = "/Users/jonasb./Desktop/azule test/ytcl.deb"
+output_dir = "/Users/jonasb./Desktop/azule test"
+
+extract_deb(deb_file, output_dir)
 
 def find_dylib_files(path):
     dylib_files = []
@@ -35,30 +45,17 @@ def find_dylib_files(path):
                 dylib_files.append(os.path.join(root, file))
     return dylib_files
 
-deb_file_path = r'C:\Users\jonas\Downloads\Telegram Desktop\com.dvntm.youtubeplus_2.3_iphoneos-arm.deb'
-output_dir = r'C:\Users\jonas\Desktop'
+extracted_dir = os.path.join(output_dir, 'Library')
+extract_deb(deb_file, extracted_dir)
 
-if platform.system == 'Windows':
-    extracted_dir = os.path.join(output_dir, 'Library')
-    extract_deb(deb_file_path, extracted_dir)
+dylib_files = find_dylib_files(extracted_dir)
 
-    dylib_files = find_dylib_files(extracted_dir)
+temp_dir = os.path.join(output_dir, 'temp')
+os.makedirs(temp_dir, exist_ok=True)
 
-    temp_dir = os.path.join(output_dir, 'temp')
-    os.makedirs(temp_dir, exist_ok=True)
+for dylib_file in dylib_files:
+    shutil.copy(dylib_file, temp_dir)
 
-    for dylib_file in dylib_files:
-        shutil.copy(dylib_file, temp_dir)
-        shutil.rmtree(extracted_dir)
-else:
-    extracted_dir = os.path.join(output_dir, 'extracted_deb')
-    extract_deb(deb_file_path, extracted_dir)
+shutil.rmtree(extracted_dir)
 
-    dylib_files = find_dylib_files(extracted_dir)
-
-    temp_dir = os.path.join(output_dir, 'temp')
-    os.makedirs(temp_dir, exist_ok=True)
-
-    for dylib_file in dylib_files:
-        shutil.copy(dylib_file, temp_dir)
-        shutil.rmtree(extracted_dir)
+dylib_files = find_dylib_files(extracted_dir)
